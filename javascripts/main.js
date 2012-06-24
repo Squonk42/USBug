f7099570e435c65b30c4af4820cfc1e17850c227
Michel Stempin <michel.stempin@wanadoo.fr>
2012-06-24 20:23:52 +0200

    Lots of changes to get a first working page!
    Signed-off-by: Michel Stempin <michel.stempin@wanadoo.fr>

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,11 @@
 $(function() {
     //initialize accordion        
-    $("#accordion").accordion({ autoHeight: false, collapsible: true, active: false });
+    $("#accordion").accordion({ autoHeight: false, collapsible: true, active: 0 });
     //set accordion header options
     $("#accordion").accordion("option", "icons", 
     { 'header': 'ui-icon-carat-1-e', 'headerSelected': 'ui-icon-carat-1-s' });
+    $('#faq h4').click(function() {
+	$(this).next().toggle();
+	return false;
+    }).next().hide();
 });
