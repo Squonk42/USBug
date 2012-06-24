e74f8b3933e2d769fd663183d018cee3f99dfd38
Michel Stempin <michel.stempin@wanadoo.fr>
2012-06-24 13:00:34 +0300

    Update gh-pages

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,7 @@
-console.log('This would be the main JS file.');
+$(function() {
+    //initialize accordion        
+    $("#accordion").accordion();
+    //set accordion header options
+    $("#accordion").accordion("option", "icons", 
+    { 'header': 'ui-icon-carat-1-e', 'headerSelected': 'ui-icon-carat-1-s' });
+});
