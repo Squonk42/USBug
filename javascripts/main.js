d17002b4259a11c0eb8d5589eb90db38cf1607cb
Michel Stempin <michel.stempin@wanadoo.fr>
2012-07-01 18:35:36 +0200

    Added picture dispay using FancyBox overlay
    Signed-off-by: Michel Stempin <michel.stempin@wanadoo.fr>

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,7 @@
-var viewer = null;
+var viewer_schematic = null;
+var viewer_layout = null;
+var view_top = null;
+var view_bottom = null;
 
 $(function() {
 
@@ -27,7 +30,28 @@ $(function() {
 			 ]});
 
     // Initialize SeaDragon Deep Zoom Schematic
-    viewer = new Seadragon.Viewer("schematic");
-    viewer.openDzi("http://cache.zoom.it/content/8R4D.dzi",
-		   '<?xml version="1.0" encoding="utf-8"?><Image TileSize="254" Overlap="1" Format="png" ServerFormat="Default" xmlns="http://schemas.microsoft.com/deepzoom/2009"><Size Width="3508" Height="2479" /></Image>');
+    viewer_schematic = new Seadragon.Viewer("schematic");
+    viewer_schematic.openDzi("http://cache.zoom.it/content/8R4D.dzi",
+			     '<?xml version="1.0" encoding="utf-8"?><Image TileSize="254" Overlap="1" Format="png" ServerFormat="Default" xmlns="http://schemas.microsoft.com/deepzoom/2009"><Size Width="3508" Height="2479" /></Image>');
+
+    // Initialize SeaDragon Deep Zoom Layout
+    viewer_layout = new Seadragon.Viewer("layout");
+    viewer_layout.openDzi("http://cache.zoom.it/content/v0f6.dzi",
+			  '<?xml version="1.0" encoding="utf-8"?><Image TileSize="254" Overlap="1" Format="png" ServerFormat="Default" xmlns="http://schemas.microsoft.com/deepzoom/2009"><Size Width="3076" Height="2038" /></Image>');
+
+    // Initialize SeaDragon Deep Zoom Board Top
+    viewer_top = new Seadragon.Viewer("board_top");
+    viewer_top.openDzi("http://cache.zoom.it/content/mlVF.dzi",
+			  '<?xml version="1.0" encoding="utf-8"?><Image TileSize="254" Overlap="1" Format="jpg" ServerFormat="Default" xmlns="http://schemas.microsoft.com/deepzoom/2009"><Size Width="2363" Height="945" /></Image>');
+
+    // Initialize SeaDragon Deep Zoom Board Bottom
+    viewer_bottom = new Seadragon.Viewer("board_bottom");
+    viewer_bottom.openDzi("http://cache.zoom.it/content/AIOz.dzi",
+			  '<?xml version="1.0" encoding="utf-8"?><Image TileSize="254" Overlap="1" Format="jpg" ServerFormat="Default" xmlns="http://schemas.microsoft.com/deepzoom/2009"><Size Width="2363" Height="945" /></Image>');
+
+    // Initialize main image fade-in
+    $("#progression").hide();
+    $("#progression").fadeIn(2000);
+
+    $("a[rel=fancybox]").fancybox();
 });
