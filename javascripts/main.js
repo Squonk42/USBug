54f9d50a562592d73a954f8c32590991e0ca0998
Michel Stempin <michel.stempin@wanadoo.fr>
2012-07-01 13:31:06 +0200

    Added SeaDragon Deep Zoom Schematic view

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,13 +1,20 @@
+var viewer = null;
+
 $(function() {
-    //initialize accordion        
+
+    // Initialize JQuery accordion        
     $("#accordion").accordion({ autoHeight: false, collapsible: true, active: 0 });
     //set accordion header options
     $("#accordion").accordion("option", "icons", 
     { 'header': 'ui-icon-carat-1-e', 'headerSelected': 'ui-icon-carat-1-s' });
+
+    // Initialize FAQ collapsible
     $('#faq h4').click(function() {
 	$(this).next().toggle();
 	return false;
     }).next().hide();
+
+    // Initialize JQuery CPU DataTable
     $('#cpu').dataTable({"bJQueryUI": true,
 			 "bPaginate": false,
 			 "bInfo": false,
@@ -18,4 +25,9 @@ $(function() {
 			 "aoColumnDefs": [ 
 			     {"bSortable": false, "aTargets": [2, 3, 4, 5, 6, 7, 8]},
 			 ]});
+
+    // Initialize SeaDragon Deep Zoom Schematic
+    viewer = new Seadragon.Viewer("schematic");
+    viewer.openDzi("http://cache.zoom.it/content/8R4D.dzi",
+		   '<?xml version="1.0" encoding="utf-8"?><Image TileSize="254" Overlap="1" Format="png" ServerFormat="Default" xmlns="http://schemas.microsoft.com/deepzoom/2009"><Size Width="3508" Height="2479" /></Image>');
 });
